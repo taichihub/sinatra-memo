571d958d5cba6cd32f219fd5c624f8c568c1d18c
taichihub <taichikuniv21@gmail.com>
2024-07-05 18:32:29 +0900

    レビュー修正

--- app.rb
+++ app.rb
@@ -12,17 +12,16 @@ helpers do
   end
 
   def find_note(id)
-    @note = @notes.find { |n| n['id'] == id.to_i }
-    halt 404, 'Note not found' unless @note
+    notes = load_notes
+    note = notes.find { |n| n['id'] == id.to_i }
+    halt 404, 'Note not found' unless note
+    note
   end
-end
-
-before do
-  @notes = load_notes
 end
 
 # トップ画面
 get '/' do
+  @notes = load_notes
   erb :index
 end
 
@@ -33,46 +32,52 @@ end
 
 # メモ詳細画面
 get '/notes/:id' do
-  find_note(params[:id])
+  @note = find_note(params[:id])
   erb :show
 end
 
 # メモ編集画面
 get '/notes/:id/edit' do
-  find_note(params[:id])
+  @note = find_note(params[:id])
   erb :edit
 end
 
 # メモ作成
 post '/notes' do
-  new_id = @notes.empty? ? 1 : @notes.last['id'] + 1
+  notes = load_notes
+  new_id = notes.empty? ? 1 : notes.last['id'] + 1
   new_note = { 'id' => new_id, 'title' => params[:title], 'content' => params[:content] }
-  @notes << new_note
-  save_notes(@notes)
+  notes << new_note
+  save_notes(notes)
   redirect '/'
 end
 
 # メモ更新
 patch '/notes/:id' do
-  find_note(params[:id])
-  @note['title'] = params[:title]
-  @note['content'] = params[:content]
-  save_notes(@notes)
+  notes = load_notes
+  note = notes.find { |n| n['id'] == params[:id].to_i }
+  note['title'] = params[:title]
+  note['content'] = params[:content]
+  save_notes(notes)
   redirect "/notes/#{params[:id]}"
 end
 
 # メモ削除
 delete '/notes/:id' do
-  find_note(params[:id])
-  @notes.reject! { |n| n['id'] == params[:id].to_i }
-  save_notes(@notes)
+  notes = load_notes
+  notes.reject! { |n| n['id'] == params[:id].to_i }
+  save_notes(notes)
   redirect '/'
 end
 
 private
 
 def load_notes
-  File.exist?(settings.notes_file) ? JSON.parse(File.read(settings.notes_file)) : []
+  if File.exist?(settings.notes_file)
+    file_content = File.read(settings.notes_file)
+    return JSON.parse(file_content) unless file_content.empty?
+  end
+  []
 end
 
 def save_notes(notes)
